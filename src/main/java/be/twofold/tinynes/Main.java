8fdf4790cb7c82387442e94b106b85982a101963
Jan De Kock <jan@twofold.be>
2022-06-05 09:30:01 +0200

    Initial commit

---
+++ src/main/java/be/twofold/tinynes/Main.java
@@ -0,0 +1,7 @@
+package be.twofold.tinynes;
+
+public final class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/test/java/be/twofold/tinynes/MainTest.java
@@ -0,0 +1,14 @@
+package be.twofold.tinynes;
+
+import org.junit.jupiter.api.*;
+
+import static org.assertj.core.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void test() {
+        assertThat(true).isTrue();
+    }
+
+}
