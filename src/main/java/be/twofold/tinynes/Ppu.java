e1241c3b9a45f6b1dd6439d12107c5fff573d734
Jan De Kock <jan@twofold.be>
2022-07-03 19:38:04 +0200

    Complete rewrite of the PPU
    Should be workable now, not accurate yet.

--- src/main/java/be/twofold/tinynes/Ppu.java
+++ src/main/java/be/twofold/tinynes/Ppu.java
@@ -1,5 +1,7 @@
 package be.twofold.tinynes;
 
+import java.util.*;
+
 public final class Ppu {
 
     // Palette
@@ -9,10 +11,7 @@ public final class Ppu {
 
     // PPU Address Logic
     private boolean latch;
-    private int ppuAddrTemp;
-    private int ppuAddr;
-    private int ppuScrollX;
-    private int ppuScrollY;
+    private boolean frameIsOdd;
 
     private int ppuCtrl;
     private int ppuMask;
@@ -32,10 +31,6 @@ public final class Ppu {
 
     // PPUCTRL
 
-    public int nameTable() {
-        return 0x2000 + ((ppuCtrl & 0x03) << 10);
-    }
-
     public int addressIncrement() {
         return (ppuCtrl & 0x04) != 0 ? 32 : 1;
     }
@@ -49,11 +44,7 @@ public final class Ppu {
     }
 
     public int spriteSize() {
-        return (ppuCtrl & 0x20) != 0 ? 16 : 8;
-    }
-
-    public boolean ppuMasterSlave() {
-        return (ppuCtrl & 0x40) != 0;
+        return 8 << ((ppuCtrl & 0x20) >> 5);
     }
 
     public boolean nmi() {
@@ -62,10 +53,6 @@ public final class Ppu {
 
 
     // PPUMASK
-
-    public boolean grayScale() {
-        return (ppuMask & 0x01) != 0;
-    }
 
     public boolean renderBackgroundLeft() {
         return (ppuMask & 0x02) != 0;
@@ -83,88 +70,350 @@ public final class Ppu {
         return (ppuMask & 0x10) != 0;
     }
 
-    public boolean emphasizeRed() {
-        return (ppuMask & 0x20) != 0;
-    }
-
-    public boolean emphasizeGreen() {
-        return (ppuMask & 0x40) != 0;
-    }
-
-    public boolean emphasizeBlue() {
-        return (ppuMask & 0x80) != 0;
+    private boolean rendering() {
+        return (ppuMask & 0x18) != 0;
     }
 
 
     // PPUSTATUS
 
-    public boolean verticalBlank() {
-        return (ppuStatus & 0x80) != 0;
-    }
-
     public void verticalBlank(boolean value) {
-        ppuStatus = (byte) ((ppuStatus & 0x7F) | (value ? 0x80 : 0x00));
-    }
-
-    public boolean spriteZeroHit() {
-        return (ppuStatus & 0x40) != 0;
+        ppuStatus = (ppuStatus & ~0x80) | (value ? 0x80 : 0);
     }
 
     public void spriteZeroHit(boolean value) {
-        ppuStatus = (byte) ((ppuStatus & 0xBF) | (value ? 0x40 : 0x00));
-    }
-
-    public boolean spriteOverflow() {
-        return (ppuStatus & 0x20) != 0;
+        ppuStatus = (ppuStatus & ~0x40) | (value ? 0x40 : 0);
     }
 
     public void spriteOverflow(boolean value) {
-        ppuStatus = (byte) ((ppuStatus & 0xDF) | (value ? 0x20 : 0x00));
+        ppuStatus = (ppuStatus & ~0x20) | (value ? 0x20 : 0);
+    }
+
+    // endregion
+
+    // region PPU Registers
+
+    private int v;
+    private int t;
+    private int x;
+
+    private int coarseX() {
+        return v & 0x1F;
+    }
+
+    private int coarseY() {
+        return (v >>> 5) & 0x1F;
+    }
+
+    public int fineY() {
+        return (v >>> 12) & 0x07;
+    }
+
+    private void tempNameTable(int value) {
+        assert value >= 0 && value <= 3;
+        t = (t & 0xF3FF) | (value << 10);
     }
 
     // endregion
 
     public void clock() {
-        if (col == 1) {
-            if (row == 241) {
-                verticalBlank(true); // Set VBlank flag
+        if (row >= 0 && row < 240 || row == 261) {
+            // Sprites
+            switch (col) {
+                case 257 -> filterSprites();
+                case 340 -> prepareSprites();
+            }
+
+            // Background
+            updateBackground();
+        }
+
+        if (row == 241)
+            if (col == 1) {
+                verticalBlank(true);
+
                 if (nmi()) {
                     nmi = true;
                 }
             }
-            if (row == 261) {
-                verticalBlank(false); // Clear VBlank flag
+
+        if (row == 261) {
+            if (col == 1) {
+                verticalBlank(false);
+                spriteOverflow(false);
+                spriteZeroHit(false);
+            }
+            if (col >= 280 && col < 305) {
+                transferY();
             }
         }
 
-        col++;
-        if (col > 340) {
+        if (++col > 340) {
             col = 0;
-            row++;
-            if (row > 261) {
+            if (++row > 261) {
                 row = 0;
+                frameIsOdd = !frameIsOdd;
             }
         }
+    }
+
+    // region Sprites
+
+    private final byte[] spriteScanline = new byte[32];
+    private final int[] spritePixels = new int[8];
+    private final int[] spriteIds = new int[8];
+
+    private void filterSprites() {
+        Arrays.fill(spriteScanline, (byte) 0xFF);
+        Arrays.fill(spritePixels, 0);
+        if (row == 261) return;
+
+        int spriteCount = 0;
+        int spriteSize = spriteSize();
+        for (int i = 0; i < 64; i++) {
+            int line = row - getSpriteY(oam, i);
+
+            if (line >= 0 && line < spriteSize) {
+                System.arraycopy(oam, i * 4, spriteScanline, spriteCount * 4, 4);
+                spriteIds[spriteCount] = i;
+
+                if (++spriteCount >= 8) {
+                    spriteOverflow(true);
+                    break;
+                }
+            }
+        }
+    }
+
+    private void prepareSprites() {
+        for (int i = 0; i < 8; i++) {
+            int spriteId = getSpriteId(spriteScanline, i);
+            int spriteSize = spriteSize();
+            int spriteAddress = spriteSize == 8
+                ? spriteTable() | spriteId << 4
+                : (spriteId & 0x01) << 12 | (spriteId & 0xFE) << 4;
+
+            int spriteY = (row - getSpriteY(spriteScanline, i)) % spriteSize;
+            if ((getSpriteAttr(spriteScanline, i) & 0x80) != 0) {
+                spriteY ^= spriteSize - 1;
+            }
+
+            int address = (spriteAddress | spriteY & 0x08) + spriteY;
+            spritePixels[i] = interleave(read(address), read(address + 8));
+        }
+    }
+
+    private int getSpriteX(byte[] table, int index) {
+        return Byte.toUnsignedInt(table[index * 4 + 3]);
+    }
+
+    private int getSpriteY(byte[] table, int index) {
+        return Byte.toUnsignedInt(table[index * 4]);
+    }
+
+    private int getSpriteId(byte[] table, int index) {
+        return Byte.toUnsignedInt(table[index * 4 + 1]);
+    }
+
+    private int getSpriteAttr(byte[] table, int index) {
+        return table[index * 4 + 2];
+    }
+
+    // endregion
+
+    // region Background
+
+    private int bgNextId;
+    private int bgNextAttr;
+    private int bgNextAddr;
+    private int bgNextLsb;
+    private int bgNextMsb;
+    private int bgShifter;
+    private int bgAttrShifter;
+
+    private void updateBackground() {
+        if ((col >= 1 && col <= 257) || (col >= 321 && col <= 337)) {
+            updateShifters();
+
+            switch ((col - 1) & 0x07) {
+                case 0 -> {
+                    loadBackgroundShifters();
+                    bgNextId = read(0x2000 | (v & 0x0FFF));
+                }
+
+                case 2 -> {
+                    int x = coarseX();
+                    int y = coarseY();
+
+                    bgNextAttr = read(0x23c0 | v & 0x0C00 | ((y >> 2) << 3) | (x >> 2));
+                    bgNextAttr >>= (y & 0x02) << 1;
+                    bgNextAttr >>= (x & 0x02);
+                    bgNextAttr &= 0x03;
+                }
+
+                case 4 -> {
+                    bgNextAddr = backgroundTable() | (bgNextId << 4) | fineY();
+                    bgNextLsb = read(bgNextAddr);
+                }
+
+                case 6 -> bgNextMsb = read(bgNextAddr + 8);
+
+                case 7 -> incrementX();
+            }
+        }
+
+        if (col == 256) {
+            incrementY();
+        }
+
+        if (col == 257) {
+            loadBackgroundShifters();
+            transferX();
+        }
+
+        if (col == 338 || col == 340) {
+            bgNextId = read(0x2000 | (v & 0x0FFF));
+        }
+
+        renderPixel();
+    }
+
+    private void loadBackgroundShifters() {
+        bgShifter = (bgShifter & 0xFFFF0000) | interleave(bgNextLsb, bgNextMsb);
+        bgAttrShifter = (bgAttrShifter & 0xFFFF0000) | (bgNextAttr * 0x5555);
+    }
+
+    private void updateShifters() {
+        if (renderBackground()) {
+            bgShifter <<= 2;
+            bgAttrShifter <<= 2;
+        }
+    }
+
+    private void incrementX() {
+        if (!rendering()) {
+            return;
+        }
+
+        if ((v & 0x001F) == 31) {
+            v &= ~0x001F;
+            v ^= 0x0400;
+            return;
+        }
+
+        v += 1;
+    }
+
+    private void incrementY() {
+        if (!rendering()) {
+            return;
+        }
+
+        if ((v & 0x7000) != 0x7000) {
+            v += 0x1000;
+            return;
+        }
+
+        v &= ~0x7000;
+        int y = coarseY();
+        if (y == 29) {
+            y = 0;
+            v ^= 0x0800;
+        } else if (y == 31) {
+            y = 0;
+        } else {
+            y++;
+        }
+        v = (v & ~0x03E0) | (y << 5);
+    }
+
+    private void transferX() {
+        if (!rendering()) {
+            return;
+        }
+        v = (v & 0x7BE0) | (t & 0x041F);
+    }
+
+    private void transferY() {
+        if (!rendering()) {
+            return;
+        }
+        v = (v & 0x041F) | (t & 0x7BE0);
+    }
+
+    // endregion
+
+    private void renderPixel() {
+        int px = col - 1;
+        int py = row;
+        if (py < 0 || py >= 240 || px < 0 || px >= 256) {
+            return;
+        }
+
+        int bgPalette = 0;
+        if (renderBackground() && (renderBackgroundLeft() || px >= 8)) {
+            int shift = 30 - (x << 1);
+            bgPalette = (bgShifter >>> shift) & 0x03;
+            if (bgPalette != 0) {
+                bgPalette |= ((bgAttrShifter >>> shift) & 0x03) << 2;
+            }
+        }
+
+        int fgPalette = 0;
+        boolean fgPriority = false;
+        if (renderSprites() && (renderSpritesLeft() || px >= 8)) {
+
+            for (int i = 0; i < 8; i++) {
+                int spriteAttr = getSpriteAttr(spriteScanline, i);
+                int spriteX = px - getSpriteX(spriteScanline, i);
+                if (spriteX < 0 || spriteX >= 8) continue;
+
+                if ((spriteAttr & 0x40) != 0) spriteX ^= 7;
+                fgPalette = (spritePixels[i] >>> ((7 - spriteX) << 1)) & 0x03;
+                if (fgPalette == 0) continue;
+
+                if (spriteIds[i] == 0 && bgPalette != 0) spriteZeroHit(true);
+                fgPalette |= ((spriteAttr & 0x03) | 0x04) << 2;
+                fgPriority = (spriteAttr & 0x20) == 0;
+                break;
+            }
+        }
+
+        int palette = priority(bgPalette, fgPalette, fgPriority);
+        frame[(py * 256) + px] = bus.read(0x3F00 | palette);
+    }
+
+    private int priority(int bgPalette, int fgPalette, boolean fgPriority) {
+        if (fgPalette == 0) return bgPalette;
+        if (bgPalette == 0) return fgPalette;
+        return fgPriority ? fgPalette : bgPalette;
     }
 
     public byte cpuRead(int address) {
         int register = address & 0x07;
 
         return switch (register) {
-            case 2 -> {
-                byte result = (byte) (ppuStatus & 0xE0);
-                latch = false;
-                verticalBlank(false);
-                yield result;
-            }
+            case 2 -> readPpuStatus();
             case 4 -> oam[oamAddr];
-            case 7 -> {
-                byte result = bus.read(ppuAddr);
-                ppuAddr += addressIncrement();
-                yield result;
-            }
+            case 7 -> readPpuData();
             default -> throw new IllegalArgumentException("Invalid register: " + (register));
         };
+    }
+
+    private byte readPpuStatus() {
+        byte result = (byte) (ppuStatus & 0xE0);
+        latch = false;
+        verticalBlank(false);
+        return result;
+    }
+
+    private byte ppuDataBuffer;
+
+    private byte readPpuData() {
+        byte result = ppuDataBuffer;
+        ppuDataBuffer = bus.read(v);
+        v += addressIncrement();
+        return v >= 0x3F00 ? ppuDataBuffer : result;
     }
 
     public void cpuWrite(int address, byte data) {
@@ -172,144 +421,68 @@ public final class Ppu {
         int value = Byte.toUnsignedInt(data);
 
         switch (register) {
-            case 0 -> ppuCtrl = value;
+            case 0 -> {
+                ppuCtrl = value;
+                tempNameTable(value & 0x03);
+            }
             case 1 -> ppuMask = value;
             case 2 -> {
             }
             case 3 -> oamAddr = value;
-            case 4 -> writeOamData(data);
+            case 4 -> {
+                oam[oamAddr] = data;
+                oamAddr = (oamAddr + 1) & 0xFF;
+            }
             case 5 -> writePpuScroll(data);
             case 6 -> writePpuAddr(data);
-            case 7 -> writePpuData(data);
+            case 7 -> {
+                bus.write(v, data);
+                v += addressIncrement();
+            }
             default ->
                 throw new UnsupportedOperationException("Writing to PPU " + Util.hex4(address) + ": " + Util.hex2(data));
         }
     }
 
-    private void writeOamData(byte value) {
-        oam[oamAddr] = value;
-        oamAddr = (oamAddr + 1) & 0xFF;
-    }
-
     private void writePpuScroll(byte value) {
+        int data = Byte.toUnsignedInt(value);
         if (!latch) {
-            ppuScrollX = Byte.toUnsignedInt(value);
+            x = data & 0x07;
+            t = t & 0x7FE0 | (data >>> 3);
         } else {
-            ppuScrollY = Byte.toUnsignedInt(value);
+            t = t & 0x0C1F | (data & 0x07) << 12 | data >>> 3 << 5;
         }
         latch = !latch;
     }
 
     private void writePpuAddr(byte value) {
         if (!latch) {
-            ppuAddrTemp = Byte.toUnsignedInt(value) << 8;
+            t = (t & 0x00FF) | ((value & 0x3F) << 8);
         } else {
-            ppuAddrTemp |= Byte.toUnsignedInt(value);
-            ppuAddr = ppuAddrTemp;
+            t = (t & 0x7F00) | (value & 0xFF);
+            v = t;
         }
         latch = !latch;
     }
 
-    private void writePpuData(byte value) {
-        bus.write(ppuAddr, value);
-        ppuAddr += addressIncrement();
-    }
-
-    private void dumpRegister(int value, String key, String name) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 8; i > 0; i--) {
-            if ((value & (1 << (i - 1))) != 0) {
-                sb.append(key.charAt(8 - i));
-            } else {
-                sb.append('-');
-            }
-        }
-        System.out.println(name + " :" + sb);
-    }
-
     public void draw(byte[] screen) {
-        if (renderBackground()) {
-            drawBackground(screen);
-        }
-        if (renderSprites()) {
-            drawSprites(screen);
-        }
+        System.arraycopy(frame, 0, screen, 0, screen.length);
     }
 
     public void reset() {
     }
 
-    private void drawBackground(byte[] pixels) {
-        for (int y = 0; y < 30; y++) {
-            for (int x = 0; x < 32; x++) {
-                renderTile(pixels, y, x);
-            }
-        }
-    }
-
-    private void renderTile(byte[] pixels, int y, int x) {
-        int tile = Byte.toUnsignedInt(bus.read(nameTable() + y * 32 + x));
-        int tileX = (tile % 16);
-        int tileY = (tile / 16);
-        int offset = (tileY * 256) + (tileX * 16);
-        int paletteAddress = nameTable() | 0x03C0 | ((y >> 2) << 3) | (x >> 2);
-        int pi = Byte.toUnsignedInt(bus.read(paletteAddress));
-        pi >>= (y & 0x02) << 1;
-        pi >>= (x & 0x02);
-        pi &= 0x03;
-        for (int r = 0, ro = y * (8 * 256); r < 8; r++, ro += 256) {
-            int tileLsb = Byte.toUnsignedInt(bus.read(backgroundTable() + (offset + r)));
-            int tileMsb = Byte.toUnsignedInt(bus.read(backgroundTable() + (offset + r + 8)));
-            int tileInt = interleave(tileLsb, tileMsb);
-            for (int c = 0, co = x * 8; c < 8; c++, co++) {
-                int colorIndex = (tileInt >> 14 - (c * 2)) & 0x03;
-                pixels[ro + co] = bus.palette[pi * 4 + colorIndex];
-            }
-        }
-    }
-
-    private void drawSprites(byte[] pixels) {
-        for (int i = 63; i >= 0; i--) {
-            int i4 = i * 4;
-            int y = Byte.toUnsignedInt(oam[i4]) + 1;
-            int index = Byte.toUnsignedInt(oam[i4 + 1]);
-            int attr = Byte.toUnsignedInt(oam[i4 + 2]);
-            int x = Byte.toUnsignedInt(oam[i4 + 3]);
-
-            renderSprite(pixels, x, y, index, attr);
-        }
-    }
-
-    private void renderSprite(byte[] pixels, int x, int y, int index, int attr) {
-        if (x + 8 > 256 || y + 8 > 240) {
-            return;
-        }
-
-        int offset = spriteTable() + (index * 16);
-        int pi = (attr & 0x03) + 4;
-
-        boolean flipX = (attr & 0x40) != 0;
-        boolean flipY = (attr & 0x80) != 0;
-
-        for (int r = 0; r < 8; r++) {
-            int rr = flipY ? 7 - r : r;
-            int ro = (y + rr) * 256;
-            int tileLsb = Byte.toUnsignedInt(bus.read(offset + rr));
-            int tileMsb = Byte.toUnsignedInt(bus.read(offset + rr + 8));
-            int tileInt = interleave(tileLsb, tileMsb);
-            for (int c = 0; c < 8; c++) {
-                int cc = flipX ? 7 - c : c;
-                int colorIndex = (tileInt >> 14 - (cc * 2)) & 0x03;
-                if (colorIndex != 0) {
-                    pixels[ro + x + c] = bus.palette[pi * 4 + colorIndex];
-                }
-            }
-        }
-    }
+    // region Old Helpers
 
     private int interleave(int lsb, int msb) {
         long lsbI = ((lsb * 0x0101010101010101L & 0x8040201008040201L) * 0x0102040810204081L >> 49) & 0x5555;
         long msbI = ((msb * 0x0101010101010101L & 0x8040201008040201L) * 0x0102040810204081L >> 48) & 0xAAAA;
         return (int) (lsbI | msbI);
+    }
+
+    // endregion
+
+    private int read(int address) {
+        return Byte.toUnsignedInt(bus.read(address));
     }
 }
